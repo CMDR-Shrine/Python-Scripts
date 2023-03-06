9b9ce22cd7ab0731b8c927a6cdf5fbe9eefcbc12
CMDR-Shrine <118768045+CMDR-Shrine@users.noreply.github.com>
2023-03-06 21:04:12 +0100

    Add files via upload

---
+++ MadLibsGame.py
@@ -0,0 +1,11 @@
+# This is a python based mad libs game
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a Celebrity: ")
+
+
+
+print("Roses are " + color )
+print(plural_noun + " are  blue ")
+print("I love " + celebrity)
